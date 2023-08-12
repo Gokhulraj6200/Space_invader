266cceb3dfe8aa7b3bff96d7634eabf98cc39226
Gokhulraj6200 <gokhulraj6200@gmail.com>
2023-08-12 11:30:34 +0200

    Added Test 1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import time
 import random
 pygame.font.init()
-
+#test1
 WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Shooter Tutorial")
